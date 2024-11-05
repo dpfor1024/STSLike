17f33a4c509029963dcc3aa01406b0707e976fb0
dpfor1024 <18236885078@163.com>
2024-11-05 22:33:44 +0800

    add:添加卡牌管理类

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CardManager : MonoBehaviour
+{
+    public GameObject CardPrefab;
+    public int Size;
+
+    private readonly Stack<GameObject> _instances = new Stack<GameObject>();
+
+
+    public void Awake()
+    {
+        Assert.IsNotNull(CardPrefab);
+    }
+
+    public void Init()
+    {
+        for (int i = 0; i < Size; i++) { 
+            var obj=CreateInstance();
+            obj.SetActive(false);
+            _instances.Push(obj);
+        }
+    }
+
+
+    private GameObject CreateInstance()
+    {
+        return Instantiate(CardPrefab,transform,true);
+    }
+}
